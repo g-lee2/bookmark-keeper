46567a12cbd9a2872850ff93041bfa87f7fa6408
Grace Lee <gracelee9901@gmail.com>
2022-12-27 10:26:05 -0800

    Add functionality that closes modal when x or outside the modal is clicked

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
-const bookmarksContainer = document.getElementById('booksmarks-container');
+const bookmarksContainer = document.getElementById('bookmarks-container');
 
 // Show Modal, Focus on Input 
 function showModal() {
@@ -14,3 +14,5 @@ function showModal() {
 
 // Modal Event Listeners 
 modalShow.addEventListener('click', showModal); 
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
